f22e27deb1d3e1299bb96fcdae95db95cdf9e956
Ld <126683779+ldisgod@users.noreply.github.com>
2024-11-13 11:54:13 +0800

    first

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import gradio as gr
 import traceback
+from typing import Tuple
+import re
 
-
-def hello_world_fn(username: str) -> tuple[str, str]:
+def hello_world_fn(username: str) -> Tuple[str, str]:
     try:
         return f"HELLO WORLD\n{username.upper()}", "SUCCESS"
     except Exception as e:
         return f"opus! some exception {e}\n{traceback.format_exc()}", "FAILED"
 
+
+def my_func(content):
+    #content = content.replace("<c>444</c>","")
+    result = re.findall("<p>.*?(\d+).*?</p>",content,re.S)
+    return '\n'.join(result)
 
 def main() -> None:
     with gr.Blocks(title="DeepLang Data test project") as demo:
@@ -34,6 +40,18 @@ def main() -> None:
                 inputs=raw_input,
                 outputs=[pack_output, status_output],
             )
+            
+        with gr.Tab("html parser"):
+            raw_input = gr.Textbox(lines=1, placeholder="", label="")
+            pack_output = gr.Textbox(label="输出")
+        
+            btn = gr.Button("开始转换")
+            btn.click(
+                fn=my_func,
+                inputs=raw_input,
+                outputs=[pack_output],
+            )
+
 
     demo.queue(default_concurrency_limit=100).launch(
         inline=False,
